ebdef814e07265d9a771da9ae79ab53544801841
CapSap <cshotam@gmail.com>
2020-08-29 14:53:23 +1000

    more fix/clean

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Colour from "./components/colour.js";
 import Board from "./components/board.js";
-import {  getLoc } from "./functions/functions.js";
+import { calculateWinner,  getLoc } from "./functions/functions.js";
 
 class Game extends React.Component {
   constructor(props) {
@@ -20,32 +20,11 @@ class Game extends React.Component {
     };
   }
 
-  calculateWinner(sq) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (sq[a] && sq[a] === sq[b] && sq[a] === sq[c]) {
-        return sq[a];
-      }
-    }
-    return null;
-  }
-
   handleClick(i) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[this.state.stepNumber];
     const squares = current.squares.slice();
     const moveLocHistory = this.state.moveLoc.slice(0, this.state.stepNumber);
-    const calculateWinner = this.calculateWinner;
 
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -74,7 +53,6 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const calculateWinner = this.calculateWinner;
     const winner = calculateWinner(current.squares);
     let makeBold = "";
 
