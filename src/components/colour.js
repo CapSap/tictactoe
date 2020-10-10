f045147cab49430c47c8c94cb77cb1cee2192828
CapSap <cshotam@gmail.com>
2020-10-10 20:28:58 +1100

    move xhr to handleClick(). fix arrow function and this.status

--- src/components/colour.js
+++ src/components/colour.js
@@ -16,23 +16,36 @@ class Colour extends React.Component {
 
   componentDidUpdate() {
 
-    var xhr = new XMLHttpRequest();
-    let url = "http://www.scolr.org/json/color/random";
-    xhr.open('GET',url,true);
-    xhr.onload = function() {
-      if(this.status == 200) {
-        var data = JSON.parse(xhr.responseText);
-        console.log(data)                      
-      } 
-    }
-    xhr.send();
+    // var xhr = new XMLHttpRequest();
+    // let url = "http://www.colr.org/json/color/random";
+    // xhr.open('GET',url,true);
+    // xhr.onload = function() {
+    //   if(this.status == 200) {
+    //     var data = JSON.parse(xhr.responseText);
+        
+    //   } 
+    // }
+    // console.log('outside onload()')
+
+    // xhr.send();
+
     }   
   
     handleClick() {
-      this.setState ({
-        updatePlease: true
-      })
-      console.log('click')
+    var xhr = new XMLHttpRequest();
+    let url = "http://www.colr.org/json/color/random";
+    xhr.open('GET',url,true);
+    xhr.onload = () => {
+      if(xhr.status == 200) {
+        var data = JSON.parse(xhr.responseText);      
+        this.setState({
+          randomColour: 'testing'
+        })        
+      } 
+      console.log(data)
+    } 
+
+    xhr.send();
     }
 
    
